30a7e192500b2e46874151199c1322a6a7331ec8
Victor Ivanov <vivanov@allot.com>
2026-07-13 17:32:35 +0300

    merge with main + added extension auth user

---
+++ src/main/java/common/annotations/AuthUser.java
@@ -0,0 +1,19 @@
+package common.annotations;
+
+import common.enums.UserRoles;
+import common.extensions.AuthUserExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@ExtendWith(AuthUserExtension.class)
+public @interface AuthUser {
+
+    UserRoles role() default UserRoles.USER_ROLE;
+
+}
--- src/main/java/common/enums/UserRoles.java
+++ src/main/java/common/enums/UserRoles.java
@@ -2,14 +2,19 @@ package common.enums;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public enum UserRoles {
 
-    PROJECT_VIEWER,
-    PROJECT_DEVELOPER,
-    SYSTEM_ADMIN,
-    PROJECT_ADMIN
+    PROJECT_VIEWER("p:_Root"),
+    PROJECT_DEVELOPER("p:_Root"),
+    SYSTEM_ADMIN("g"),
+    PROJECT_ADMIN("p:_Root"),
+    USER_ROLE("p:_Root")
+    ;
 
+    private String scope;
 }
