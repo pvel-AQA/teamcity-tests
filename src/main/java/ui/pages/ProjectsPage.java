03a8d6876b139f34a02423c9ebb537598e404b29
PavelV <202444749+pvel-AQA@users.noreply.github.com>
2026-07-24 00:10:20 +0300

    add retry to checkHeaderIsVisible() method

--- src/main/java/ui/pages/ProjectsPage.java
+++ src/main/java/ui/pages/ProjectsPage.java
@@ -2,8 +2,8 @@ package ui.pages;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import common.helpers.RetryUtils;
 
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selenide.$;
@@ -13,9 +13,11 @@ import static com.codeborne.selenide.WebDriverConditions.urlContaining;
 public class ProjectsPage extends BasePage<ProjectsPage> {
 
     private static final String LOGIN_PAGE_MARKER = "login.html";
+    private static final String PROJECTS_ICON_VALUE = "Projects";
 
     private final SelenideElement loginUsernameField = $("#username");
     private final SelenideElement header = $(Selectors.byXpath("//header[@data-test-main-nav]"));
+    private final SelenideElement projectsHeaderIcon = $(Selectors.byXpath("//span[text()='Projects']"));
 
     @Override
     public String url() {
@@ -29,7 +31,11 @@ public class ProjectsPage extends BasePage<ProjectsPage> {
     }
 
     public ProjectsPage checkHeaderIsVisible() {
-        Selenide.sleep(10000);
+        RetryUtils.retry("Wait until header is visible",
+                projectsHeaderIcon::getText,
+                value -> value.equals(PROJECTS_ICON_VALUE),
+                3,
+                3000);
         header.shouldBe(Condition.visible);
         return this;
     }
