d78ee3ca273755a08d0d01862edf9f02887b1dd2
nkashyrina <nkashyrina@gmail.com>
2026-07-28 09:04:34 -0500

    Implemented test that projects are displayed on Admin page

--- src/test/java/ui/AdminProjectsTest.java
+++ src/test/java/ui/AdminProjectsTest.java
@@ -22,7 +22,7 @@ public class AdminProjectsTest extends BaseUiTest {
     @AuthUser(role = UserRoles.SYSTEM_ADMIN, seedBrowserSession = true)
     void adminCanOpenAdminProjectsPageAndSeeTopProjectsInfoTest() {
 
-        //create projects
+        UserSteps.createProject();
         AdminProjectsPage adminProjectsPage = new AdminProjectsPage().open()
                 .checkItIsCorrectPage()
                 .checkHeaderIsVisible();
