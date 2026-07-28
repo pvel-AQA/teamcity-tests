967e02458eb0fa2f5fd15c135a8a951054e67b6c
Ildar Zalyalov <id10997@yandex.ru>
2026-07-28 12:35:40 +0300

    added tests for create project

--- src/main/java/ui/enums/errors/ProjectValidationError.java
+++ src/main/java/ui/enums/errors/ProjectValidationError.java
@@ -8,7 +8,9 @@ import lombok.Getter;
 public enum ProjectValidationError {
 
     PROJECT_NAME_CANNOT_BE_EMPTY("The field can't be empty"),
-    INVALID_PROJECT_ID("Invalid project ID. IDs must start with a Latin letter, be no longer than 225 characters, and contain only Latin letters, numbers, and underscores.");
+    INVALID_PROJECT_ID("Invalid project ID. IDs must start with a Latin letter, be no longer than 225 characters, and contain only Latin letters, numbers, and underscores."),
+    PROJECT_WITH_THIS_NAME_ALREADY_EXISTS("A project with this name already exists"),
+    PROJECT_WITH_THIS_ID_ALREADY_EXIESTS("A project with this ID already exists");
 
     private final String errorMsg;
 
--- src/test/java/ui/project/CreateProjectTest.java
+++ src/test/java/ui/project/CreateProjectTest.java
@@ -3,12 +3,14 @@ package ui.project;
 
 import api.comparison.ModelAssertions;
 import api.generators.RandomGenerator;
+import api.generators.TeamCityDataGenerator;
 import api.models.project.ProjectRequest;
 import api.models.project.ProjectResponse;
 import api.steps.UserSteps;
 import com.codeborne.selenide.Condition;
 import common.annotations.AuthUser;
 import common.enums.UserRoles;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ui.BaseUiTest;
 import ui.elements.ProjectElement;
@@ -16,14 +18,15 @@ import ui.pages.ConnectVCSPage;
 import ui.pages.CreateProjectPage;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static ui.enums.errors.ProjectValidationError.INVALID_PROJECT_ID;
-import static ui.enums.errors.ProjectValidationError.PROJECT_NAME_CANNOT_BE_EMPTY;
+import static ui.enums.errors.ProjectValidationError.*;
 
 
 public class CreateProjectTest extends BaseUiTest {
 
     private final static String INVALID_ID_TO_BE_GENERATED = "id";
     private final static String INVALID_NAME_TO_BE_GENERATED = "name";
+
+    private final static int EXPECTED_COUNT = 1;
 
     @Test
     @AuthUser(role = UserRoles.SYSTEM_ADMIN, seedBrowserSession = true)
@@ -81,6 +84,40 @@ public class CreateProjectTest extends BaseUiTest {
                 .anyMatch(project -> project.getId().equals(projectRequest.getId()));
 
         assertThat(projectExists).isFalse();
+    }
+
+    @Test
+    @AuthUser(role = UserRoles.SYSTEM_ADMIN, seedBrowserSession = true)
+    public void userCannotCreateProjectWithNameAlreadyExistsTest() {
+        var projectResponse = UserSteps.createProject();
+
+        new CreateProjectPage()
+                .open()
+                .createProject(projectResponse.getName(), projectResponse.getId(), projectResponse.getDescription())
+                .getProjectNameError().shouldBe(Condition.visible)
+                .shouldHave(Condition.text(PROJECT_WITH_THIS_NAME_ALREADY_EXISTS.getErrorMsg()));
+
+        var listOfProjects = UserSteps.getAllProjects().getProjects().stream()
+                .filter(project -> project.getId().equals(projectResponse.getId())).toList();
+
+        Assertions.assertThat(listOfProjects).hasSize(EXPECTED_COUNT);
+    }
+
+    @Test
+    @AuthUser(role = UserRoles.SYSTEM_ADMIN, seedBrowserSession = true)
+    public void userCannotCreateProjectWithIDAlreadyExistsTest() {
+        var projectResponse = UserSteps.createProject();
+
+        new CreateProjectPage()
+                .open()
+                .createProject(TeamCityDataGenerator.generateString(), projectResponse.getId(), projectResponse.getDescription())
+                .getProjectIdError().shouldBe(Condition.visible)
+                .shouldHave(Condition.text(PROJECT_WITH_THIS_ID_ALREADY_EXIESTS.getErrorMsg()));
+
+        var listOfProjects = UserSteps.getAllProjects().getProjects().stream()
+                .filter(project -> project.getId().equals(projectResponse.getId())).toList();
+
+        Assertions.assertThat(listOfProjects).hasSize(EXPECTED_COUNT);
     }
 
 
