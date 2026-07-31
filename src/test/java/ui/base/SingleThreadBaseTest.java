2044af4bc2d16458e64211267edb0d23de6da6ba
PavelV <202444749+pvel-AQA@users.noreply.github.com>
2026-07-31 15:18:10 +0300

    fix timeout issue debug

--- src/test/java/ui/base/SingleThreadBaseTest.java
+++ src/test/java/ui/base/SingleThreadBaseTest.java
@@ -2,7 +2,9 @@ package ui.base;
 
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.api.parallel.Isolated;
 
-@Execution(ExecutionMode.SAME_THREAD)
+//@Execution(ExecutionMode.SAME_THREAD)
+@Isolated
 public class SingleThreadBaseTest extends BaseUiTest {
 }
