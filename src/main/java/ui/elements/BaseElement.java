b72b7180c18d2c27168d3edc4ede2b2f5b32efcf
PavelV <202444749+pvel-AQA@users.noreply.github.com>
2026-07-31 14:42:31 +0300

    fix clickCreateUnderProjectButton() method

--- src/main/java/ui/elements/BaseElement.java
+++ src/main/java/ui/elements/BaseElement.java
@@ -25,7 +25,7 @@ public abstract class BaseElement {
     protected void retryUntilElementIsDisplayed(SelenideElement element) {
         RetryUtils.retry(
                 "Wait until web element is displayed",
-                () -> element.isDisplayed() && element.isEnabled(),
+                () -> element.isDisplayed(),
                 visible -> visible,
                 60,
                 1000
