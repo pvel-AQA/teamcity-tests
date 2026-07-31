5e05a9ca97cbe226642e425c91fe7c7a43da81ef
Ildar Zalyalov <id10997@yandex.ru>
2026-07-31 16:39:17 +0300

    debug skip button

--- src/main/java/ui/pages/CreateBuildConfigurationPage.java
+++ src/main/java/ui/pages/CreateBuildConfigurationPage.java
@@ -4,15 +4,32 @@ import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.back;
+import static java.lang.Thread.sleep;
 
 public class CreateBuildConfigurationPage extends BasePage<CreateBuildConfigurationPage> {
 
     private final SelenideElement skipButton = $(Selectors.byText("Skip"));
 
-    public EditProjectPage clickSkipButton() {
-      skipButton.click();
-      return getPage(EditProjectPage.class);
+    public EditProjectPage clickSkipButton() throws InterruptedException {
+        int maxAttempts = 2;
+        for (int i = 0; i < maxAttempts; i++) {
+            try {
+                skipButton.click();
+                break;
+            } catch (Exception | AssertionError e) {
+                if (i == maxAttempts - 1) {
+                    throw e;
+                }
+                back();
+                sleep(1000);
+                getPage(ConnectVCSPage.class).clickProceedWithoutRepositoryButton();
+                sleep(1000);
+            }
+        }
+        return getPage(EditProjectPage.class);
     }
+
 
     @Override
     public String url() {
