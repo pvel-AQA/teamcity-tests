83e564d0ccb9d614e0739107f7090bc3b8a19609
Ildar Zalyalov <id10997@yandex.ru>
2026-08-05 16:45:40 +0300

    multi-browsers run

--- src/test/java/ui/base/BaseUiTest.java
+++ src/test/java/ui/base/BaseUiTest.java
@@ -5,9 +5,11 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import common.configs.Config;
+import io.qameta.allure.Allure;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.util.Map;
 
@@ -15,16 +17,23 @@ public class BaseUiTest extends BaseTest {
 
     @BeforeAll
     public static void setupSelenoid() {
-        Configuration.remote = Config.getProperty("uiRemote");
-        Configuration.baseUrl = Config.getProperty("uiBaseUrl");
-        Configuration.browser = Config.getProperty("browser");
+        String browser = System.getProperty("browser", Config.getProperty("browser"));
+        Configuration.remote = System.getProperty("uiRemote", Config.getProperty("uiRemote"));
+        Configuration.baseUrl = System.getProperty("uiBaseUrl", Config.getProperty("uiBaseUrl"));
+        Configuration.browser = browser;
         Configuration.browserSize = Config.getProperty("browserSize");
-        Configuration.headless = Boolean.parseBoolean(Config.getProperty("headless"));
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         Configuration.browserCapabilities.setCapability("selenoid:options",
                 Map.of("enableVNC", true, "enableLog", true)
         );
+    }
+
+    @BeforeEach
+    public void setupTestAllureParams() {
+        // Записываем параметр именно перед СТАРТОМ КАЖДОГО ТЕСТА
+        Allure.parameter("Browser", Configuration.browser);
     }
 
     @AfterEach
