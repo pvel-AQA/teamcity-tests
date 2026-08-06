f66602fbd79edc44e4ba5e535e223f0ea80205c5
Ildar Zalyalov <id10997@yandex.ru>
2026-08-06 16:08:21 +0300

    multi-browser-run

--- src/test/java/ui/base/BaseUiTest.java
+++ src/test/java/ui/base/BaseUiTest.java
@@ -3,10 +3,8 @@ package ui.base;
 import base.BaseTest;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import common.configs.Config;
 import io.qameta.allure.Allure;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,18 +16,16 @@ public class BaseUiTest extends BaseTest {
 
     @BeforeAll
     public static void setupSelenoid() {
-        String browser = System.getProperty("browser", Config.getProperty("browser"));
+        Configuration.browser = System.getProperty("browser", Config.getProperty("browser"));
         Configuration.remote = System.getProperty("uiRemote", Config.getProperty("uiRemote"));
-
         Configuration.baseUrl = System.getProperty("uiBaseUrl", Config.getProperty("uiBaseUrl"));
-        Configuration.browser = browser;
         Configuration.browserSize = Config.getProperty("browserSize");
 
-        Configuration.timeout = 10000;
+//        Configuration.timeout = 10000;
 
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
-                .screenshots(true)
-                .savePageSource(true));
+//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+//                .screenshots(true)
+//                .savePageSource(true));
 
         Configuration.browserCapabilities.setCapability("selenoid:options",
                 Map.of("enableVNC", true, "enableLog", true)
@@ -45,6 +41,7 @@ public class BaseUiTest extends BaseTest {
             testCase.setName(displayName);
             testCase.setHistoryId(testCase.getHistoryId() + "-" + browser);
         });
+
         Allure.parameter("Browser", browser);
     }
 
