da47ec6bb498d0498a660662d6504f99570469b0
Victor Ivanov <vivanov@allot.com>
2026-08-07 16:35:41 +0300

    trying to use listener

---
+++ src/main/java/common/extensions/BrowserAllureListener.java
@@ -0,0 +1,26 @@
+package common.extensions;
+
+import common.configs.Config;
+import io.qameta.allure.listener.TestLifecycleListener;
+import io.qameta.allure.model.Label;
+import io.qameta.allure.model.TestResult;
+
+public class BrowserAllureListener implements TestLifecycleListener {
+    @Override
+    public void beforeTestSchedule(TestResult result) {
+        String browser = System.getProperty("browser");
+        if (browser == null || browser.isBlank()) {
+            browser = Config.getProperty("browser");
+        }
+        // 1. Меняем имя, чтобы визуально тесты отличались в списке
+        result.setName(result.getName() + " [" + browser + "]");
+        // 2. Строим правильный historyId, чтобы тесты не склеивались
+        result.setHistoryId(result.getHistoryId() + "-" + browser);
+        // 3. Добавляем Label для вашего allurerc.js
+        result.getLabels().add(new Label().setName("browser").setValue(browser));
+        // 4. Добавляем как параметр (опционально, для красоты в UI)
+        result.getParameters().add(new io.qameta.allure.model.Parameter()
+                .setName("browser")
+                .setValue(browser));
+    }
+}
--- src/test/java/ui/base/BaseUiTest.java
+++ src/test/java/ui/base/BaseUiTest.java
@@ -42,14 +42,14 @@ public class BaseUiTest extends BaseTest {
             Configuration.browser = browser;
         }
 
-        Allure.getLifecycle().updateTestCase(testResult -> {
+        /*Allure.getLifecycle().updateTestCase(testResult -> {
             testResult.setName(testResult.getName() + " [" + browser + "]");
             testResult.setHistoryId(testResult.getHistoryId() + "-" + browser);
-            /*testResult.getLabels().add(new io.qameta.allure.model.Label()
+            testResult.getLabels().add(new io.qameta.allure.model.Label()
                     .setName("browser")
-                    .setValue(browser));*/
+                    .setValue(browser));
         });
-        Allure.parameter("browser", browser);
+        Allure.parameter("browser", browser);*/
 
     }
 
