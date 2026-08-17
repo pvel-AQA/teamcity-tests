1e71c81668a0a8c1c98319171799ca81ca63552e
Victor Ivanov <vivanov@allot.com>
2026-08-17 14:37:18 +0300

    trying magic with allure 3 history

--- allurerc.js
+++ allurerc.js
@@ -6,14 +6,7 @@ export default defineConfig({
     historyPath: "allure-history/history.jsonl",
     appendHistory: true,
     output: "./allure-report", // <-- Это дефолт для локального запуска. В CI он перекроется флагом --output
-    widgets: [
-        {
-            type: "testBaseGrowthDynamics",
-            title: "Test base growth dynamics",
-            statuses: ["passed", "failed", "broken", "skipped", "unknown"],
-            limit: 10,
-        }
-    ],
+
     // Quality Gates - автоматическая проверка качества тестового прогона
     qualityGate: {
         rules: [
